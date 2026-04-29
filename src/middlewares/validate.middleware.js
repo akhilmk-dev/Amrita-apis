15b60c393a218db44c93316da72bf05fe70b7da6
Akhil M K <akhil7594876187@gmail.com>
2026-04-29 16:12:27 +0530

    image upload bug issue

--- src/middlewares/validate.middleware.js
+++ src/middlewares/validate.middleware.js
@@ -7,11 +7,16 @@ import { ApiError } from '../utils/response.utils.js';
  */
 export const validate = (schema) => (req, res, next) => {
   try {
-    schema.parse({
+    const parsedData = schema.parse({
       body: req.body,
       query: req.query,
       params: req.params,
     });
+
+    if (parsedData.body) req.body = parsedData.body;
+    if (parsedData.query) req.query = parsedData.query;
+    if (parsedData.params) req.params = parsedData.params;
+
     next();
   } catch (error) {
     if (error instanceof z.ZodError) {
